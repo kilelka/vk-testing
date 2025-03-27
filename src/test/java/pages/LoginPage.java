bfbd0b8025e7b44919172c82f6716f696ecf2ca2
kilelka <kogan.oa@mail.ru>
2025-03-27 17:38:22 +0300

    Last

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -11,15 +11,13 @@ public class LoginPage {
     private final SelenideElement passwordInput = $x("//*[@id='field_password']");
     private final SelenideElement loginButton = $x("//form//input[@type='submit' or @value='Войти']");
 
-    // Вводим логин и пароль
     public LoginPage enterCredentials(String username, String password) {
-        usernameInput.shouldBe(visible).setValue(username);  // Ожидаем появления поля и вводим логин
-        passwordInput.shouldBe(visible).setValue(password);  // Ожидаем появления поля и вводим пароль
-        return this;  // Возвращаем текущий объект для продолжения цепочки вызовов
+        usernameInput.shouldBe(visible).setValue(username);
+        passwordInput.shouldBe(visible).setValue(password);
+        return this;
     }
 
-    // Нажимаем кнопку "Войти"
     public void clickLogin() {
-        loginButton.shouldBe(visible).click();  // Ожидаем видимости кнопки и нажимаем на нее
+        loginButton.shouldBe(visible).click();
     }
 }
